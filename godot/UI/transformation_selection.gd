eb22e1c04c155ef2ce06e100883d7c4217742699
thegouge <thegougedev@gmail.com>
2026-05-09 03:25:25 -0700

    Closing the transformation menu on selection

--- godot/UI/transformation_selection.gd
+++ godot/UI/transformation_selection.gd
@@ -1,19 +1,31 @@
 extends Control
 
+@onready var Trans_menu_animation: AnimationPlayer = %TransMenuAnimation
+
 signal transform
 
 func _on_basic_pressed() -> void:
 	transform.emit(Global.ObjectTypes.BASIC)
+	close()
 
 func _on_magical_pressed() -> void:
 	transform.emit(Global.ObjectTypes.MAGICAL)
+	close()
 
 func _on_cursed_pressed() -> void:
 	transform.emit(Global.ObjectTypes.CURSED)
+	close()
 
 func _on_locked_pressed() -> void:
 	transform.emit(Global.ObjectTypes.LOCKED)
+	close()
 
 func _on_heavy_pressed() -> void:
 	transform.emit(Global.ObjectTypes.HEAVY)
+	close()
 
+func close() -> void:
+	Trans_menu_animation.play_backwards("open")
+
+func open() -> void:
+	Trans_menu_animation.play("open")
