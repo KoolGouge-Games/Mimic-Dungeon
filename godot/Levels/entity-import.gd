435941dfb03a76b39d64e7a5988a2e63b9efb5c0
thegouge <thegougedev@gmail.com>
2026-05-11 20:33:21 -0700

    finally got the import scripts where I like them

--- godot/Levels/entity-import.gd
+++ godot/Levels/entity-import.gd
@@ -12,47 +12,31 @@ func post_import(entity_layer: LDTKEntityLayer) -> LDTKEntityLayer:
 		match entity.identifier:
 			"Player_Spawn":
 				var player_scene = PLAYER.instantiate()
-				var camera_node = Camera2D.new()
-				var remote_node = RemoteTransform2D.new()
-				player_scene.add_child(remote_node)
-
 				player_scene.global_position = entity.position
-
-				entity_layer.add_child(camera_node)
 				entity_layer.add_child(player_scene)
 			"NPC_Spawn":
 				var marker := Marker2D.new()
 				marker.global_position = entity.position 
 				entity_layer.add_child(marker)
 			"Item":
-				var itemScene := process_item(entity)
-				entity_layer.add_child(itemScene)
+				var interactable_scene: Interactable = INTERACTABLE.instantiate()
+
+				print(entity.fields.Item)
+
+				match entity.fields.Item:
+					"Item_Type.Cursed":
+						interactable_scene.ObjectType = Global.ObjectTypes.CURSED
+					"Item_Type.Heavy":
+						interactable_scene.ObjectType = Global.ObjectTypes.HEAVY
+					"Item_Type.Locked":
+						interactable_scene.ObjectType = Global.ObjectTypes.LOCKED
+					"Item_Type.Magic":
+						interactable_scene.ObjectType = Global.ObjectTypes.MAGICAL
+					"Item_Type.Basic_Crate":
+						interactable_scene.ObjectType = Global.ObjectTypes.BASIC
+
+				interactable_scene.global_position = entity.position
+				entity_layer.add_child(interactable_scene)
 
 	return entity_layer
-
-func process_item(entity) -> Interactable:
-	var interactable_scene: Interactable = INTERACTABLE.instantiate()
-	var texture_path := "res://assets/objects/%s.png"
-
-	match entity.fields.Item:
-		"Item_type.Cursed":
-			interactable_scene.ObjectType = Global.ObjectTypes.CURSED
-			texture_path = texture_path % "Cursed"
-		"Item_type.Heavy":
-			interactable_scene.ObjectType = Global.ObjectTypes.HEAVY
-			texture_path = texture_path % "Heavy"
-		"Item_type.Locked":
-			interactable_scene.ObjectType = Global.ObjectTypes.LOCKED
-			texture_path = texture_path % "Locked"
-		"Item_type.Magic":
-			interactable_scene.ObjectType = Global.ObjectTypes.MAGICAL
-			texture_path = texture_path % "Magic"
-		_:
-			interactable_scene.ObjectType = Global.ObjectTypes.BASIC
-			texture_path = texture_path % "Basic"
-
-	interactable_scene.get_child(0).texture = load(texture_path)
-	
-	interactable_scene.global_position = entity.position
-	return interactable_scene
 	
